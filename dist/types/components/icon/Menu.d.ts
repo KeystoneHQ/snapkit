50f05ee92602082ee2c2e84236a07abf890344c9
rea <hi@rea.ink>
2022-09-14 14:00:02 +0800

    Published by publish-to-git 3c3065e5b76a488f35d446ad7b52d3023cdc3ac0 (HEAD -> refs/heads/dev, refs/remotes/origin/dev) publish: v0.1.2

---
+++ dist/types/components/icon/Menu.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { IconProps } from './types';
+export declare function Menu({ className, width, height, color }: IconProps): JSX.Element;
+//# sourceMappingURL=Menu.d.ts.map
