dab5ea844818a9f6b140b7aa28f0d6455f98fcd3
rea <hi@rea.ink>
2022-09-14 13:47:12 +0800

    fix: export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,9 @@ export { Icon } from './icon'
 export { Button } from './base/Button'
 export { Loading } from './base/Loading'
 export { Modal } from './base/Modal'
+export { Balance } from './balance'
+export { ActionButton } from './balance/ActionButton'
+export { Tabs } from './Tabs'
+export { Tab } from './Tabs/Tab'
 
 export default {}
