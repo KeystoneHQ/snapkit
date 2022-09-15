ddf352ee0c714132e11f4551e6185764ea2c925c
rea <hi@rea.ink>
2022-09-15 10:30:44 +0800

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        presets: ['@babel/preset-typescript'],
-        plugins: [
-          '@babel/plugin-transform-typescript',
-          [
-            'babel-plugin-styled-components',
-            {
-              ssr: false,
-              pure: true,
-              displayName: true,
-              fileName: false
-            }
-          ]
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   build: {
     lib: {
       entry: './src/index.ts',
