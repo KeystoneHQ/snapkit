8fe4d7ef0de8e1001065c4bae0daf261bda431e4
rea <hi@rea.ink>
2022-09-29 10:25:57 +0800

    Published by publish-to-git fe73d784befc352ccbcdbb43e2196756138f5db7 (HEAD -> refs/heads/snapkit-v1, refs/remotes/origin/snapkit-v1) publish: tag 0.1.10

---
+++ dist/types/components/icon/ArrowRight.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { IconProps } from './types';
+export declare function ArrowRight({ className, width, height, color }: IconProps): JSX.Element;
+//# sourceMappingURL=ArrowRight.d.ts.map
---
+++ dist/types/components/icon/Receive.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { IconProps } from './types';
+export declare function Receive({ className, width, height, color }: IconProps): JSX.Element;
+//# sourceMappingURL=Receive.d.ts.map
