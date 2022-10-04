9e443e74ad6164764a244d77df864fda6b512e15
YanZhe <zhe@keyst.one>
2022-10-04 22:37:58 +0800

    Published by publish-to-git 8a3cd44e022799b8f4458254d3304ed984c81ade (HEAD -> refs/heads/dev, refs/remotes/origin/dev) delete span css

---
+++ dist/types/components/icon/Connect.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { IconProps } from './types';
+export declare function Connect({ className, width, height, color }: IconProps): JSX.Element;
+//# sourceMappingURL=Connect.d.ts.map
---
+++ dist/types/components/icon/NFT.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { IconProps } from './types';
+export declare function NFT({ className, width, height, color }: IconProps): JSX.Element;
+//# sourceMappingURL=NFT.d.ts.map
