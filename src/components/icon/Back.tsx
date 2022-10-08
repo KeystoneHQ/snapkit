3481109dff16da7db23eba3c3e62ef331fd84678
rea <hi@rea.ink>
2022-10-08 09:30:33 +0800

    fix: icon default color, use black

--- src/components/icon/Back.tsx
+++ src/components/icon/Back.tsx
@@ -1,9 +1,22 @@
 import { IconProps } from './types'
 
-export function Back ({ className, width = '24', height = '24', color }: IconProps) {
+export function Back({
+  className,
+  width = '24',
+  height = '24',
+  color = 'black'
+}: IconProps) {
   return (
-    <svg width={width} height={height} className={className} style={{stroke: 'currentColor'}} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 5L5 12M5 12L12 19M5 12H20" stroke={color} strokeWidth="1.5"/>
+    <svg
+      width={width}
+      height={height}
+      className={className}
+      style={{ stroke: 'currentColor' }}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M12 5L5 12M5 12L12 19M5 12H20" stroke={color} strokeWidth="1.5" />
     </svg>
   )
 }
--- src/components/icon/Close.tsx
+++ src/components/icon/Close.tsx
@@ -1,9 +1,22 @@
 import { IconProps } from './types'
 
-export function Close ({ className, width = '24', height = '24', color }: IconProps) {
+export function Close({
+  className,
+  width = '24',
+  height = '24',
+  color = 'black'
+}: IconProps) {
   return (
-    <svg width={width} height={height} className={className} style={{stroke: 'currentColor'}} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M6 6L18 18M18 6L6 18" stroke={color} strokeWidth="1.5"/>
+    <svg
+      width={width}
+      height={height}
+      className={className}
+      style={{ stroke: 'currentColor' }}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M6 6L18 18M18 6L6 18" stroke={color} strokeWidth="1.5" />
     </svg>
   )
 }
